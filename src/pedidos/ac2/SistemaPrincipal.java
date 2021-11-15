933df9b8746208f66934fc6d93895d16b78fb8b7
leonardo <leonardodimarchi@outlook.com>
2021-11-15 14:00:18 -0300

    feat(paying): added a method to pay a order and separating reports menu

--- src/pedidos/ac2/SistemaPrincipal.java
+++ src/pedidos/ac2/SistemaPrincipal.java
@@ -68,180 +68,18 @@ public class SistemaPrincipal {
                     }
                     break;
                 case 5:
-
+                    try {
+                        baixaDePagamento(pedidos);
+                    } catch (Exception error) {
+                        System.out.println("\n" + error.getMessage());
+                    }
                     break;
                 case 6:
                     do {
                         imprimirMenu(opcoesMenuRelatorio, "Relatorios");
                         opcaoRelatorio = receberOpcaoInt();
 
-                        switch (opcaoRelatorio) {
-                            case 1:
-                                imprimirLinhaComTitulo("Lista de clientes");
-
-                                if (clientes.isEmpty())
-                                    System.out.println("\nNenhum cliente cadastrado");
-                                else
-                                    clientes.forEach(Cliente::imprimir);
-                                break;
-                            case 2:
-                                imprimirLinhaComTitulo("Lista de Fornecedores");
-
-                                if (fornecedores.isEmpty())
-                                    System.out.println("\nNenhum fornecedor cadastrado");
-                                else
-                                    fornecedores.forEach(Fornecedor::imprimir);
-                                break;
-                            case 3:
-                                imprimirLinhaComTitulo("Lista de Produtos");
-
-                                if (produtos.isEmpty())
-                                    System.out.println("\nNenhum produto cadastrado");
-                                else
-                                    produtos.forEach(Produto::imprimir);
-                                break;
-                            case 4:
-                                imprimirLinhaComTitulo("Lista de Pedidos");
-
-                                if (pedidos.isEmpty())
-                                    System.out.println("\nNenhum pedido cadastrado");
-                                else
-                                    pedidos.forEach(pedido -> {
-                                        pedido.imprimir();
-                                        imprimirLinha();
-                                    });
-                                break;
-                            case 5:
-                                imprimirLinhaComTitulo("Lista pedidos por data");
-                                if (pedidos.isEmpty()) {
-                                    System.out.println("\nNenhum pedido cadastrado");
-                                    break;
-                                }
-
-                                try {
-                                    System.out.print("\nDigite a data inicial (dd/MM/yyyy): ");
-                                    String dataInicialDigitada = inputScanner.next();
-
-                                    System.out.print("\nDigite a data final (dd/MM/yyyy): ");
-                                    String dataFinalDigitada = inputScanner.next();
-
-                                    Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse(dataInicialDigitada);
-                                    Date dataFinal = new SimpleDateFormat("dd/MM/yyyy").parse(dataFinalDigitada);
-
-                                    DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault());
-
-                                    List<Pedido> listaFiltrada = pedidos.stream()
-                                            .filter(pedido ->  pedido.getData().after(dataInicial) && pedido.getData().before(dataFinal))
-                                            .collect(Collectors.toList());
-
-                                    if (listaFiltrada.isEmpty())
-                                        System.out.println("\nNenhum pedido encontrado para o intervalo de datas: " +
-                                                dateFormat.format(dataInicial) + " e " + dateFormat.format(dataFinal));
-                                    else
-                                        listaFiltrada.forEach(Pedido::imprimir);
-
-                                } catch (ParseException errorParsing) {
-                                    System.out.println("\nPor favor, utilizar o formato especificado...");
-                                } catch (Exception error) {
-                                    System.out.println("\nOcorreu um erro inesperado... Tente novamente.");
-                                }
-
-                                break;
-                            case 6:
-                                imprimirLinhaComTitulo("Buscar pedido pelo numero");
-
-                                if (pedidos.isEmpty()) {
-                                    System.out.println("\nNenhum pedido cadastrado");
-                                    break;
-                                }
-
-                                System.out.print("\nDigite o identificador desejado: ");
-                                int identificadorBuscado = inputScanner.nextInt();
-
-                                Pedido pedidoSelecionado = null;
-
-                                for (Pedido pedido : pedidos) {
-                                    if (pedido.getIdentificador() == identificadorBuscado)
-                                        pedidoSelecionado = pedido;
-                                }
-
-                                if (pedidoSelecionado == null)
-                                    System.out.println("\nNenhum pedido encontrado com esse identificador");
-                                else
-                                    pedidoSelecionado.imprimir();
-
-                                break;
-                            case 7:
-                                imprimirLinhaComTitulo("Lista de pedidos pagos");
-
-                                if (pedidos.isEmpty()) {
-                                    System.out.println("\nNenhum pedido cadastrado");
-                                    break;
-                                }
-
-                                List<Pedido> pedidosPagos = pedidos.stream()
-                                        .filter(Pedido::estaPago)
-                                        .collect(Collectors.toList());
-
-                                if (pedidosPagos.isEmpty())
-                                    System.out.println("\nNenhum pedido esta pago no momento");
-                                else
-                                    pedidosPagos.forEach(Pedido::imprimir);
-
-                                break;
-                            case 8:
-                                imprimirLinhaComTitulo("Busca de produto pelo nome");
-
-                                if (produtos.isEmpty()) {
-                                    System.out.println("\nNenhum produto cadastrado");
-                                    break;
-                                }
-
-                                inputScanner.nextLine();
-                                System.out.print("\nDigite o nome do produto: ");
-                                String nomeProcurado = inputScanner.nextLine();
-
-                                List<Produto> produtosEncontrados = produtos.stream()
-                                        .filter(produto -> produto.getNome().toLowerCase().contains(nomeProcurado.toLowerCase()))
-                                        .collect(Collectors.toList());
-
-                                if (produtosEncontrados.isEmpty())
-                                    System.out.println("\nNão foram encontrados produtos contendo este nome");
-                                else
-                                    produtosEncontrados.forEach(Produto::imprimir);
-
-                                break;
-                            case 9:
-                                imprimirLinhaComTitulo("Calculo do total de pedidos em aberto (não pagos)");
-
-                                if (pedidos.isEmpty()) {
-                                    System.out.println("\nNenhum pedido cadastrado");
-                                    break;
-                                }
-
-                                float valorTotalEmAberto = 0;
-
-                                List<Pedido> pedidosEmAberto = pedidos.stream()
-                                        .filter(pedido -> !pedido.estaPago())
-                                        .collect(Collectors.toList());
-
-                                if (pedidosEmAberto.isEmpty()) {
-                                    System.out.println("\nNenhum pedido em aberto");
-                                    break;
-                                }
-
-                                for (Pedido pedido : pedidosEmAberto)
-                                    valorTotalEmAberto += pedido.getValorTotal();
-
-                                System.out.println("\nValor total de pedidos em aberto: " + valorTotalEmAberto);
-
-                                break;
-                            case 10:
-                                System.out.println("\nSaindo do menu de relatorio...");
-                                break;
-                            default:
-                                System.out.println("\nSelecione uma opcao valida de relatorio");
-                        }
+                        selecionarOpcaoDeRelatorio(opcaoRelatorio, clientes, fornecedores, produtos, pedidos);
                     } while (opcaoRelatorio != opcoesMenuRelatorio.length);
                     break;
                 case 7:
@@ -253,8 +91,211 @@ public class SistemaPrincipal {
         } while (opcaoSelecionada != opcoesDoMenuPrincipal.length);
     }
 
-    private static void baixaDePagamento() {
+    private static void selecionarOpcaoDeRelatorio(int opcaoRelatorio, List<Cliente> clientes, List<Fornecedor> fornecedores, List<Produto> produtos, List<Pedido> pedidos) {
+        switch (opcaoRelatorio) {
+            case 1:
+                imprimirLinhaComTitulo("Lista de clientes");
 
+                if (clientes.isEmpty())
+                    System.out.println("\nNenhum cliente cadastrado");
+                else
+                    clientes.forEach(Cliente::imprimir);
+                break;
+            case 2:
+                imprimirLinhaComTitulo("Lista de Fornecedores");
+
+                if (fornecedores.isEmpty())
+                    System.out.println("\nNenhum fornecedor cadastrado");
+                else
+                    fornecedores.forEach(Fornecedor::imprimir);
+                break;
+            case 3:
+                imprimirLinhaComTitulo("Lista de Produtos");
+
+                if (produtos.isEmpty())
+                    System.out.println("\nNenhum produto cadastrado");
+                else
+                    produtos.forEach(Produto::imprimir);
+                break;
+            case 4:
+                imprimirLinhaComTitulo("Lista de Pedidos");
+
+                if (pedidos.isEmpty())
+                    System.out.println("\nNenhum pedido cadastrado");
+                else
+                    pedidos.forEach(pedido -> {
+                        pedido.imprimir();
+                        imprimirLinha();
+                    });
+                break;
+            case 5:
+                imprimirLinhaComTitulo("Lista pedidos por data");
+                if (pedidos.isEmpty()) {
+                    System.out.println("\nNenhum pedido cadastrado");
+                    break;
+                }
+                try {
+                    System.out.print("\nDigite a data inicial (dd/MM/yyyy): ");
+                    String dataInicialDigitada = inputScanner.next();
+
+                    System.out.print("\nDigite a data final (dd/MM/yyyy): ");
+                    String dataFinalDigitada = inputScanner.next();
+
+                    Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse(dataInicialDigitada);
+                    Date dataFinal = new SimpleDateFormat("dd/MM/yyyy").parse(dataFinalDigitada);
+
+                    DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault());
+
+                    List<Pedido> listaFiltrada = pedidos.stream()
+                            .filter(pedido ->  pedido.getData().after(dataInicial) && pedido.getData().before(dataFinal))
+                            .collect(Collectors.toList());
+
+                    if (listaFiltrada.isEmpty())
+                        System.out.println("\nNenhum pedido encontrado para o intervalo de datas: " +
+                                dateFormat.format(dataInicial) + " e " + dateFormat.format(dataFinal));
+                    else
+                        listaFiltrada.forEach(Pedido::imprimir);
+
+                } catch (ParseException errorParsing) {
+                    System.out.println("\nPor favor, utilizar o formato especificado...");
+                } catch (Exception error) {
+                    System.out.println("\nOcorreu um erro inesperado... Tente novamente.");
+                }
+
+                break;
+            case 6:
+                imprimirLinhaComTitulo("Buscar pedido pelo numero");
+
+                if (pedidos.isEmpty()) {
+                    System.out.println("\nNenhum pedido cadastrado");
+                    break;
+                }
+
+                System.out.print("\nDigite o identificador desejado: ");
+                int identificadorBuscado = inputScanner.nextInt();
+
+                try {
+                    Pedido pedidoSelecionado = encontrarPedidoPorIndentificador(pedidos, identificadorBuscado);
+                    pedidoSelecionado.imprimir();
+                } catch (Exception error) {
+                    System.out.println("\n" + error.getMessage());
+                }
+
+                break;
+            case 7:
+                imprimirLinhaComTitulo("Lista de pedidos pagos");
+
+                if (pedidos.isEmpty()) {
+                    System.out.println("\nNenhum pedido cadastrado");
+                    break;
+                }
+
+                List<Pedido> pedidosPagos = pedidos.stream()
+                        .filter(Pedido::estaPago)
+                        .collect(Collectors.toList());
+
+                if (pedidosPagos.isEmpty())
+                    System.out.println("\nNenhum pedido esta pago no momento");
+                else
+                    pedidosPagos.forEach(Pedido::imprimir);
+
+                break;
+            case 8:
+                imprimirLinhaComTitulo("Busca de produto pelo nome");
+
+                if (produtos.isEmpty()) {
+                    System.out.println("\nNenhum produto cadastrado");
+                    break;
+                }
+
+                inputScanner.nextLine();
+                System.out.print("\nDigite o nome do produto: ");
+                String nomeProcurado = inputScanner.nextLine();
+
+                List<Produto> produtosEncontrados = produtos.stream()
+                        .filter(produto -> produto.getNome().toLowerCase().contains(nomeProcurado.toLowerCase()))
+                        .collect(Collectors.toList());
+
+                if (produtosEncontrados.isEmpty())
+                    System.out.println("\nNão foram encontrados produtos contendo este nome");
+                else
+                    produtosEncontrados.forEach(Produto::imprimir);
+
+                break;
+            case 9:
+                imprimirLinhaComTitulo("Calculo do total de pedidos em aberto (não pagos)");
+
+                if (pedidos.isEmpty()) {
+                    System.out.println("\nNenhum pedido cadastrado");
+                    break;
+                }
+
+                float valorTotalEmAberto = 0;
+
+                List<Pedido> pedidosEmAberto = pedidos.stream()
+                        .filter(pedido -> !pedido.estaPago())
+                        .collect(Collectors.toList());
+
+                if (pedidosEmAberto.isEmpty()) {
+                    System.out.println("\nNenhum pedido em aberto");
+                    break;
+                }
+
+                for (Pedido pedido : pedidosEmAberto)
+                    valorTotalEmAberto += pedido.getValorTotal();
+
+                System.out.println("\nValor total de pedidos em aberto: " + valorTotalEmAberto);
+
+                break;
+            case 10:
+                System.out.println("\nSaindo do menu de relatorio...");
+                break;
+            default:
+                System.out.println("\nSelecione uma opcao valida de relatorio");
+        }
+    }
+
+    private static Pedido encontrarPedidoPorIndentificador(List<Pedido> pedidos, int identificadorBuscado) throws Exception {
+        Pedido pedidoSelecionado = null;
+
+        for (Pedido pedido : pedidos) {
+            if (pedido.getIdentificador() == identificadorBuscado)
+                pedidoSelecionado = pedido;
+        }
+
+        if (pedidoSelecionado == null)
+            throw new Exception("Nenhum pedido encontrado com esse identificador");
+        else
+            return pedidoSelecionado;
+    }
+
+    private static void baixaDePagamento(List<Pedido> pedidos) throws Exception {
+        imprimirLinhaComTitulo("Efetuar baixa de pagamento de pedido");
+
+        if (pedidos.isEmpty())
+            throw new Exception("Por favor, cadastre ao menos 1 pedido para efetuar a baixa de pagamento");
+
+        int identificadorDesejado;
+        List<Pedido> pedidosEmAberto = pedidos.stream()
+                .filter(pedido -> !pedido.estaPago())
+                .collect(Collectors.toList());
+
+        pedidosEmAberto.forEach(pedido -> {
+            pedido.imprimir();
+            imprimirLinha();
+        });
+
+        System.out.print("\nDigite o identificador desejado: ");
+        identificadorDesejado = inputScanner.nextInt();
+
+        try {
+            Pedido pedidoEncontrado = encontrarPedidoPorIndentificador(pedidosEmAberto, identificadorDesejado);
+            pedidoEncontrado.setEstaPago(true);
+            pedidoEncontrado.imprimir();
+            System.out.println("\nBaixa de pagamento feito com sucesso");
+        } catch (Exception error) {
+            throw new Exception(error.getMessage());
+        }
     }
 
     private static Pedido efetuarPedidoComIdentificador(int identificador, List<Cliente> clientes, List<Produto> produtos) throws Exception {
